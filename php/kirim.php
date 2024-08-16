001a17799052d926dcd0bb7a2821bd16f032d780
langdev <langzzstorelaxy@gmail.com>
2024-08-16 23:07:42 +0700

    Update kirim.php

--- php/kirim.php
+++ php/kirim.php
@@ -1,1 +1,22 @@
+<?php
+$nama = $_POST['nama'];
+$mapel = $_POST['mapel'];
+$kelas = $_POST['kelas'];
+$catatan = $_POST['note'];
 
+$pesan = "
+FORMULIR PESANAN JOKI
+――――――――――――――――――――――
+- Nama : ".$nama."
+- Mapel : ".$mapel."
+- Kelas : ".$kelas."
+- Catatan : ".$catatan."
+";
+
+$encodePesan = urlencode($pesan);
+
+$nomor = "628123456789"; // ubah pake nomor whatsapp awalan 62
+$url = "https://api.whatsapp.com/send?phone={$nomor}&text={$encodePesan}";
+
+header("Location: $url");
+exit();
